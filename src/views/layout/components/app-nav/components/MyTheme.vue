db7fd7c4781063bc996289242a906afc358006b1
baojunGit <1018440268@qq.com>
2022-05-13 21:22:10 +0800

    add: 增加主题配置抽屉

--- src/views/layout/components/app-nav/components/MyTheme.vue
+++ src/views/layout/components/app-nav/components/MyTheme.vue
@@ -1,7 +1,29 @@
 <template>
   <div class="my-theme">
-    <i class="iconfont icon-tubiao02-xianxing"></i>
+    <i class="iconfont icon-tubiao02-xianxing" @click="openDrawer"></i>
+    <!-- append-to-body 让层级最高 -->
+    <el-drawer
+      title="主题配置"
+      v-model="drawer"
+      direction="rtl"
+      size="450px"
+      append-to-body
+    >
+      11111
+    </el-drawer>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { reactive, toRefs } from 'vue'
+
+const state = reactive({
+  drawer: false
+})
+
+const openDrawer = () => {
+  state.drawer = true
+}
+
+const { drawer } = toRefs(state)
+</script>
