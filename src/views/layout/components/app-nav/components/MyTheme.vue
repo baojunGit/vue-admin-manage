d63d70497a387cee780f4c37a1cd085022474c18
baojunGit <1018440268@qq.com>
2022-05-14 23:41:04 +0800

    perf: 更改主题配置

--- src/views/layout/components/app-nav/components/MyTheme.vue
+++ src/views/layout/components/app-nav/components/MyTheme.vue
@@ -6,10 +6,63 @@
       title="主题配置"
       v-model="drawer"
       direction="rtl"
-      size="450px"
+      size="300px"
       append-to-body
     >
-      11111
+      <el-scrollbar height="88vh">
+        <el-form ref="form" label-position="left" :model="theme">
+          <el-form-item label="主题">
+            <el-select v-model="theme.themeName" @change="updateTheme">
+              <el-option
+                key="blue-black"
+                :label="translateTitle('蓝黑')"
+                value="blue-black"
+              />
+              <el-option
+                key="blue-white"
+                :label="translateTitle('蓝白')"
+                value="blue-white"
+              />
+              <el-option
+                key="green-black"
+                :label="translateTitle('绿黑')"
+                value="green-black"
+              />
+              <el-option
+                key="green-white"
+                :label="translateTitle('绿白')"
+                value="green-white"
+              />
+              <el-option
+                key="purple-black"
+                :label="translateTitle('紫黑')"
+                value="purple-black"
+              />
+              <el-option
+                key="purple-white"
+                :label="translateTitle('紫白')"
+                value="purple-white"
+              />
+              <!-- 红黑、红白主题完成群文档任务免费获取 -->
+              <el-option
+                key="red-black"
+                :label="translateTitle('红黑')"
+                value="red-black"
+              />
+              <el-option
+                key="red-white"
+                :label="translateTitle('红白')"
+                value="red-white"
+              />
+              <el-option
+                key="ocean"
+                :label="translateTitle('渐变')"
+                value="ocean"
+              />
+            </el-select>
+          </el-form-item>
+        </el-form>
+      </el-scrollbar>
     </el-drawer>
   </div>
 </template>
@@ -23,6 +76,10 @@ const state = reactive({
 
 const openDrawer = () => {
   state.drawer = true
+}
+
+const updateTheme = () => {
+  console.log(111)
 }
 
 const { drawer } = toRefs(state)
