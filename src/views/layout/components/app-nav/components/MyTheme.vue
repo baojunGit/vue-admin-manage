bd0dc4db830237c88bd7588d6afaf95491e712ed
baojunGit <1018440268@qq.com>
2022-05-17 20:43:24 +0800

    perf: 新增默认主题色选择

--- src/views/layout/components/app-nav/components/MyTheme.vue
+++ src/views/layout/components/app-nav/components/MyTheme.vue
@@ -12,7 +12,10 @@
       <el-scrollbar height="88vh">
         <div class="demo-color-block">
           <span class="demonstration">主题色更换</span>
-          <el-color-picker v-model="theme.themeColor" />
+          <el-color-picker
+            v-model="theme.themeColor"
+            :predefine="predefineColors"
+          />
         </div>
         <el-button @click="updateTheme">确定</el-button>
       </el-scrollbar>
@@ -21,16 +24,10 @@
 </template>
 
 <script setup lang="ts">
-import { reactive, toRefs } from 'vue'
+import { reactive, toRefs, ref } from 'vue'
 import { useSettingStore } from '@/store/modules/setting'
 import { storeToRefs } from 'pinia'
 import { generateNewStyle, writeNewStyle } from '@/utils/theme'
-
-const settingStore = useSettingStore()
-
-const { updateThemeColor } = settingStore
-
-const { theme } = storeToRefs(settingStore)
 
 const state = reactive({
   drawer: false
@@ -39,6 +36,29 @@ const state = reactive({
 const openDrawer = () => {
   state.drawer = true
 }
+
+const settingStore = useSettingStore()
+
+const { updateThemeColor } = settingStore
+
+const { theme } = storeToRefs(settingStore)
+
+const predefineColors = ref([
+  '#ff4500',
+  '#ff8c00',
+  '#ffd700',
+  '#90ee90',
+  '#00ced1',
+  '#1e90ff',
+  '#c71585',
+  'rgba(255, 69, 0, 0.68)',
+  'rgb(255, 120, 0)',
+  'hsv(51, 100, 98)',
+  'hsva(120, 40, 94, 0.5)',
+  'hsl(181, 100%, 37%)',
+  'hsla(209, 100%, 56%, 0.73)',
+  '#c7158577'
+])
 
 const updateTheme = async () => {
   const newStyle = await generateNewStyle(theme.value.themeColor)
