e3f7277b4bce505848c59c3d92db046771d712c8
baojunGit <1018440268@qq.com>
2022-09-16 16:52:38 +0800

    feat: 增加loading组件

--- src/views/Edit/Article/index.vue
+++ src/views/Edit/Article/index.vue
@@ -10,7 +10,7 @@ import {
 	nextTick
 } from 'vue';
 import { Editor, Toolbar } from '@wangeditor/editor-for-vue';
-import { IToolbarConfig } from '@wangeditor/editor';
+// import { IToolbarConfig } from '@wangeditor/editor';
 import '@wangeditor/editor/dist/css/style.css'; // 引入 css
 import { Search } from '@element-plus/icons-vue';
 import type { ElTree, ElInput } from 'element-plus';
@@ -30,66 +30,66 @@ onMounted(() => {
 });
 
 // 将toolbarConfig设置为空对象，默认模式下就会加载所有的功能组件
-// const toolbarConfig = {};
-const toolbarConfig: Partial<IToolbarConfig> = {
-	toolbarKeys: [
-		// 菜单 key
-		'headerSelect',
+const toolbarConfig = {};
+// const toolbarConfig: Partial<IToolbarConfig> = {
+// 	toolbarKeys: [
+// 		// 菜单 key
+// 		'headerSelect',
 
-		// 分割线
-		'|',
+// 		// 分割线
+// 		'|',
 
-		// 菜单 key
-		'fontSize',
-		'fontFamily',
-		'lineHeight',
-		'bold',
-		'underline',
-		'italic',
-		'through',
-		'clearStyle',
-		'color',
-		'bgColor',
-		// 菜单组，包含多个菜单
-		{
-			key: 'group-more-justify', // 必填，要以 group 开头
-			title: '对齐方式', // 必填
-			iconSvg:
-				'<svg viewBox="0 0 1024 1024"><path d="M768 793.6v102.4H51.2v-102.4h716.8z m204.8-230.4v102.4H51.2v-102.4h921.6z m-204.8-230.4v102.4H51.2v-102.4h716.8zM972.8 102.4v102.4H51.2V102.4h921.6z"></path></svg>', // 可选
-			menuKeys: [
-				'justifyLeft',
-				'justifyRight',
-				'justifyCenter',
-				'justifyJustify'
-			] // 下级菜单 key ，必填
-		},
-		{
-			key: 'group-more-indent',
-			title: '缩进配置',
-			iconSvg:
-				'<svg viewBox="0 0 1024 1024"><path d="M0 64h1024v128H0z m384 192h640v128H384z m0 192h640v128H384z m0 192h640v128H384zM0 832h1024v128H0z m0-128V320l256 192z"></path></svg>',
-			menuKeys: ['indent', 'delIndent']
-		},
-		'insertLink',
-		{
-			key: 'group-more-image',
-			title: '图片配置',
-			iconSvg:
-				'<svg viewBox="0 0 1024 1024"><path d="M959.877 128l0.123 0.123v767.775l-0.123 0.122H64.102l-0.122-0.122V128.123l0.122-0.123h895.775zM960 64H64C28.795 64 0 92.795 0 128v768c0 35.205 28.795 64 64 64h896c35.205 0 64-28.795 64-64V128c0-35.205-28.795-64-64-64zM832 288.01c0 53.023-42.988 96.01-96.01 96.01s-96.01-42.987-96.01-96.01S682.967 192 735.99 192 832 234.988 832 288.01zM896 832H128V704l224.01-384 256 320h64l224.01-192z"></path></svg>', // 可选
-			menuKeys: ['insertImage', 'uploadImage']
-		},
-		'insertTable',
-		'insertVideo',
-		{
-			key: 'group-more-style',
-			title: '更多样式',
-			iconSvg:
-				'<svg viewBox="0 0 1024 1024"><path d="M204.8 505.6m-76.8 0a76.8 76.8 0 1 0 153.6 0 76.8 76.8 0 1 0-153.6 0Z"></path><path d="M505.6 505.6m-76.8 0a76.8 76.8 0 1 0 153.6 0 76.8 76.8 0 1 0-153.6 0Z"></path><path d="M806.4 505.6m-76.8 0a76.8 76.8 0 1 0 153.6 0 76.8 76.8 0 1 0-153.6 0Z"></path></svg>', // 可选
-			menuKeys: ['code']
-		},
-		'fullScreen'
-	]
-};
+// 		// 菜单 key
+// 		'fontSize',
+// 		'fontFamily',
+// 		'lineHeight',
+// 		'bold',
+// 		'underline',
+// 		'italic',
+// 		'through',
+// 		'clearStyle',
+// 		'color',
+// 		'bgColor',
+// 		// 菜单组，包含多个菜单
+// 		{
+// 			key: 'group-more-justify', // 必填，要以 group 开头
+// 			title: '对齐方式', // 必填
+// 			iconSvg:
+// 				'<svg viewBox="0 0 1024 1024"><path d="M768 793.6v102.4H51.2v-102.4h716.8z m204.8-230.4v102.4H51.2v-102.4h921.6z m-204.8-230.4v102.4H51.2v-102.4h716.8zM972.8 102.4v102.4H51.2V102.4h921.6z"></path></svg>', // 可选
+// 			menuKeys: [
+// 				'justifyLeft',
+// 				'justifyRight',
+// 				'justifyCenter',
+// 				'justifyJustify'
+// 			] // 下级菜单 key ，必填
+// 		},
+// 		{
+// 			key: 'group-more-indent',
+// 			title: '缩进配置',
+// 			iconSvg:
+// 				'<svg viewBox="0 0 1024 1024"><path d="M0 64h1024v128H0z m384 192h640v128H384z m0 192h640v128H384z m0 192h640v128H384zM0 832h1024v128H0z m0-128V320l256 192z"></path></svg>',
+// 			menuKeys: ['indent', 'delIndent']
+// 		},
+// 		'insertLink',
+// 		{
+// 			key: 'group-more-image',
+// 			title: '图片配置',
+// 			iconSvg:
+// 				'<svg viewBox="0 0 1024 1024"><path d="M959.877 128l0.123 0.123v767.775l-0.123 0.122H64.102l-0.122-0.122V128.123l0.122-0.123h895.775zM960 64H64C28.795 64 0 92.795 0 128v768c0 35.205 28.795 64 64 64h896c35.205 0 64-28.795 64-64V128c0-35.205-28.795-64-64-64zM832 288.01c0 53.023-42.988 96.01-96.01 96.01s-96.01-42.987-96.01-96.01S682.967 192 735.99 192 832 234.988 832 288.01zM896 832H128V704l224.01-384 256 320h64l224.01-192z"></path></svg>', // 可选
+// 			menuKeys: ['insertImage', 'uploadImage']
+// 		},
+// 		'insertTable',
+// 		'insertVideo',
+// 		{
+// 			key: 'group-more-style',
+// 			title: '更多样式',
+// 			iconSvg:
+// 				'<svg viewBox="0 0 1024 1024"><path d="M204.8 505.6m-76.8 0a76.8 76.8 0 1 0 153.6 0 76.8 76.8 0 1 0-153.6 0Z"></path><path d="M505.6 505.6m-76.8 0a76.8 76.8 0 1 0 153.6 0 76.8 76.8 0 1 0-153.6 0Z"></path><path d="M806.4 505.6m-76.8 0a76.8 76.8 0 1 0 153.6 0 76.8 76.8 0 1 0-153.6 0Z"></path></svg>', // 可选
+// 			menuKeys: ['code']
+// 		},
+// 		'fullScreen'
+// 	]
+// };
 const editorConfig = { placeholder: '请输入内容...' };
 
 // 组件销毁时，也及时销毁编辑器
@@ -176,7 +176,7 @@ const {
 
 <template>
 	<el-row :gutter="20">
-		<el-col :xl="5" :lg="5" :md="5" :sm="24" :xs="24">
+		<el-col :span="5">
 			<el-card>
 				<template #header>
 					<el-input v-model="name" placeholder="请输入文章名">
@@ -198,7 +198,7 @@ const {
 				></el-tree>
 			</el-card>
 		</el-col>
-		<el-col :xl="19" :lg="19" :md="19" :sm="24" :xs="24">
+		<el-col :span="19">
 			<el-card>
 				<template #header>
 					<el-tag
