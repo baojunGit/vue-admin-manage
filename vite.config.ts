5dbbe3ba44399e0a9b72767fea8f9e828e73956d
baojunGit <18006996859@163.com>
2024-08-21 17:34:49 +0800

    feature: 分包，性能优化

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,11 @@ import vue from '@vitejs/plugin-vue'
 // VueSetupExtend扩展 <script setup> 语法的功能, 如直接定义组件的名称
 // 如：<script setup name="MyComponent">
 import vueSetupExtend from 'vite-plugin-vue-setup-extend'
-// 默认情况下，vite-plugin-pwa将缓存所有的js、css和html文件
+// 分析打包内容
+import { visualizer } from 'rollup-plugin-visualizer'
+// 使用gzip压缩是为了减少服务端向客户端所传输文件的大小
+// 浏览器解压文件也需要时间，其成本可能大于传输中节约的时间，因此对于过小的文件不应该使用gzip压缩
+import viteCompression from 'vite-plugin-compression'
 import { resolve } from 'path'
 import dayjs from 'dayjs'
 import pkg from './package.json'
@@ -26,8 +30,12 @@ export default ({ mode }: ConfigEnv): UserConfigExport => {
     plugins: [
       vue(),
       vueSetupExtend(),
+      visualizer({ open: true }), // 打包完成后自动打开报告页面
+      viteCompression({
+        threshold: 102400, // 对大于 100kb 的文件进行压缩
+      })
     ],
-    base: './',
+    base: '/', // 默认就是/
     resolve: {
       alias: [
         {
@@ -61,10 +69,57 @@ export default ({ mode }: ConfigEnv): UserConfigExport => {
       // }
     },
     // define定义全局常量替换方式。其中每项在开发环境下会被定义在全局，而在构建时被静态替换。
+    // 需要在global.d.ts里定义
     define: {
       // string 值会以原始表达式形式使用，所以如果定义了一个字符串常量，
       // 它需要被显式地打引号。（例如使用 JSON.stringify）
       __APP_INFO__: JSON.stringify(__APP_INFO__)
+    },
+    // 打包上线配置项
+    build: {
+      target: 'es2015',
+      sourcemap: false,
+      // 消除打包大小超过500kb警告，设置为 4000 KB（即 4 MB）
+      chunkSizeWarningLimit: 4000,
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            // vue的核心库单独分包，要放在最前面
+            if (
+              id.includes('node_modules/vue') ||
+              id.includes('node_modules/vue-router') ||
+              id.includes('node_modules/pinia') ||
+              id.includes('node_modules/vue-i18n')
+            ) {
+              return 'vue'
+            } else if (
+              id.includes('node_modules/element-plus') ||
+              id.includes('node_modules/@element-plus/icons-vue')
+            ) {
+              return 'element-plus'
+            } else if (id.includes('node_modules/echarts')) {
+              return 'echarts'
+            } else if (id.includes('node_modules/vuedraggable') || id.includes('node_modules/sortablejs')) {
+              return 'drag-drop'
+            } else if (
+              id.includes('node_modules/@wangeditor/editor') ||
+              id.includes('node_modules/@wangeditor/editor-for-vue')
+            ) {
+              return 'wangeditor'
+            }else if(id.includes('node_modules/vxe-table') ||
+            id.includes('node_modules/xe-utils')){
+              return 'vxe-table'
+            }
+            else if (id.includes('node_modules')) {
+              // 其他未匹配到的第三方依赖包统一打包为 vendors
+              return 'vendors';
+            } else {
+              // 对于项目中的其他代码，可以根据路径或模块类型进一步分包
+              return 'index';
+            }
+          }
+        }
+      }
     }
   }
 }
