343c1d5bec7c91ba2e29cd39f257a37bd5e9d5d7
baojunGit <18006996859@163.com>
2024-08-22 00:54:05 +0800

    feature: 规范图片目录管理

---
+++ src/config/getImg.ts
@@ -0,0 +1,25 @@
+/**
+ * 图片地址管理
+ */
+
+// global
+import error401 from '@/assets/images/global/401.png';
+import error404 from '@/assets/images/global/404.png';
+import cloudImg from '@/assets/images/global/cloud.png';
+import checkcodeImg from '@/assets/images/global/checkcode.png';
+import faceImg from '@/assets/images/global/face.gif';
+
+// login
+import logoImg from '@/assets/images/login/logo.jpg';
+
+export {
+    // global
+    error401,
+    error404,
+    cloudImg,
+    checkcodeImg,
+    faceImg,
+
+    // login
+    logoImg
+}
