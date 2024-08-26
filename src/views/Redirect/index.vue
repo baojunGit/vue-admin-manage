92d2f701695121b4b3f4818d8faba0f49ab89a7b
baojunGit <18006996859@163.com>
2024-08-26 17:17:16 +0800

    fix: 设置重定向页面id样式，避免页面底部内容往上显示闪烁

--- src/views/Redirect/index.vue
+++ src/views/Redirect/index.vue
@@ -1,20 +1,20 @@
 <script setup lang="ts">
-import { unref } from 'vue';
-import { useRouter } from 'vue-router';
+import { unref } from 'vue'
+import { useRouter } from 'vue-router'
 
-const { currentRoute, replace } = useRouter();
+const { currentRoute, replace } = useRouter()
 
-const { params, query } = unref(currentRoute);
-const { path } = params;
+const { params, query } = unref(currentRoute)
+const { path } = params
 
-const _path = Array.isArray(path) ? path.join('/') : path;
+const _path = Array.isArray(path) ? path.join('/') : path
 
 replace({
-	path: '/' + _path,
-	query
-});
+  path: '/' + _path,
+  query
+})
 </script>
 
 <template>
-	<div />
+  <div id="redirect-container"></div>
 </template>
